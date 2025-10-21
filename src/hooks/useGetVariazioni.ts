c1d385e0a1f4c629fdb594643744729d7fb83b08
Vincenzo AppFactory <vincenzo.locatelli@appfactory.it>
2025-10-21 12:49:29 +0200

    wip var

--- src/hooks/useGetVariazioni.ts
+++ src/hooks/useGetVariazioni.ts
@@ -2,18 +2,21 @@ import { useQuery } from '@tanstack/react-query';
 import { Variazione } from 'src/types/Variazione';
 import axiosInstance from 'src/utils/axios';
 import { API_BASE_PREFIX } from 'src/utils/const';
+import { useWorkspace } from 'src/context/WorkspaceContext';
 
 
 
-const fetchVariazioni = async (prodotto_id: string): Promise<Variazione[]> => {
-    const response = await axiosInstance.get(API_BASE_PREFIX + '/products/' + prodotto_id + '/variations', {
+const fetchVariazioni = async (prodotto_id: string, ecommerceId: number): Promise<Variazione[]> => {
+    const response = await axiosInstance.get(`${API_BASE_PREFIX}/${ecommerceId}/products/${prodotto_id}/variations`, {
     });
     return response.data.data.items as Variazione[] || [];
 };
 
 export function useGetVariazioni(prodotto_id: string) {
+    const { ecommerceId } = useWorkspace();
     return useQuery({
         queryKey: ['variazioni', prodotto_id.toString()],
-        queryFn: () => fetchVariazioni(prodotto_id),
+        queryFn: () => fetchVariazioni(prodotto_id, ecommerceId as number),
+        enabled: !!ecommerceId && !!prodotto_id,
     });
 } 
